993616df64afd232efaf2ce5e1cfb5f529cad2ec
Khaled Alzeer <kalzyr61@gmail.com>
2025-08-06 16:03:27 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,36 @@
+// for image1
+var randomNumber1 = Math.floor((Math.random() * 6)) + 1; // 1-6
+
+var randomDiceImage1 = "dice" + randomNumber1 + ".png"; // dice1.png-dice6.png
+
+var randomImageSource1 = "./images/" + randomDiceImage1; // ./images/dice1.png - ./images/dice6.png
+
+var image1 = document.querySelectorAll("img")[0];
+
+image1.setAttribute("src", randomImageSource1);
+
+// for image2
+var randomNumber2 = Math.floor((Math.random() * 6)) + 1; // 1-6
+
+var randomImageSource2 = "./images/dice" + randomNumber2 + ".png";// ./images/dice1.png - ./images/dice6.png
+
+document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
+
+/*
+The way that is shorter for images
+document.querySelectorAll("img")[1].setAttribute("src", "./images/dice" + Math.floor((Math.random() * 6)) + 1 + ".png");
+*/ 
+
+//if Player 1 Wins!
+if (randomNumber1 > randomNumber2) {
+    document.querySelector("h1").innerHTML = "🚩Player 1 Wins!";
+}
+//if Player 2 Wins!
+else if (randomNumber2 > randomNumber1) {
+    document.querySelector("h1").innerHTML = "Player 2 Wins!🚩";
+}
+//if the player 1 and player 2 draw
+else {
+    document.querySelector("h1").innerHTML = "Draw!";
+}
+
